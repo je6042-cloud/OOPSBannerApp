c4538c7539b100ddbc54357acb3a8c2a92d1e7e3
je6042-cloud <je6042@srmist.edu.in>
2026-02-17 08:53:11 +0530

    UC5 - Inline Array Initialization using String.join

---
+++ UC3BannerApp.java
@@ -0,0 +1,46 @@
+public class UC3BannerApp {
+    public static void main(String[] args) {
+
+        System.out.println(String.join(" ",
+                "  ***  ",
+                "  ***  ",
+                " ******",
+                " ******"));
+
+        System.out.println(String.join(" ",
+                " ** ** ",
+                " ** ** ",
+                " **    ",
+                " **    "));
+
+        System.out.println(String.join(" ",
+                "**   **",
+                "**   **",
+                " **    ",
+                " **    "));
+
+        System.out.println(String.join(" ",
+                "**   **",
+                "**   **",
+                " ******",
+                " ******"));
+
+        System.out.println(String.join(" ",
+                "**   **",
+                "**   **",
+                " **    ",
+                "     **"));
+
+        System.out.println(String.join(" ",
+                " ** ** ",
+                " ** ** ",
+                " **    ",
+                "     **"));
+
+        System.out.println(String.join(" ",
+                "  ***  ",
+                "  ***  ",
+                " ******",
+                " ******"));
+    }
+}
