5a8c681b3a2ca3110a0f4f6a336b7d90427e121f
je6042-cloud <je6042@srmist.edu.in>
2026-03-11 08:37:54 +0530

    UC8 implemented using HashMap for pattern storage

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,13 +1,62 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class OOPSBannerApp {
+
+    static Map<Character, String[]> patternMap = new HashMap<>();
+
+    public static void loadPatterns() {
+
+        patternMap.put('O', new String[]{
+                " *** ",
+                "*   *",
+                "*   *",
+                "*   *",
+                " *** "
+        });
+
+        patternMap.put('P', new String[]{
+                "**** ",
+                "*   *",
+                "**** ",
+                "*    ",
+                "*    "
+        });
+
+        patternMap.put('S', new String[]{
+                " ****",
+                "*    ",
+                " *** ",
+                "    *",
+                "**** "
+        });
+    }
+
+    public static void renderBanner(String word) {
+
+        int height = 5;
+
+        for (int i = 0; i < height; i++) {
+
+            for (char c : word.toCharArray()) {
+
+                String[] pattern = patternMap.get(c);
+
+                if (pattern != null) {
+                    System.out.print(pattern[i] + " ");
+                }
+            }
+
+            System.out.println();
+        }
+    }
 
     public static void main(String[] args) {
 
-        System.out.println("   ***      ***    ******      *****");
-        System.out.println("  ** **    ** **   **   **    **    **");
-        System.out.println(" **   **  **   **  **   **   **");
-        System.out.println(" **   **  **   **  ******     ****");
-        System.out.println(" **   **  **   **  **             **");
-        System.out.println("  ** **    ** **   **       **    **");
-        System.out.println("   ***      ***    **        *****");
+        loadPatterns();
+
+        String word = "OOPS";
+
+        renderBanner(word);
     }
 }
